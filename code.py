6c63dceac32d138213ffb87ff3f94dc3a43d655e
Abigail Trapp <abbsthepandahead@gmail.com>
2022-11-25 18:58:51 -0700

    added example using a quotient

--- code.py
+++ code.py
@@ -5,6 +5,9 @@ print(4*2)
 
 # Built-in functions that result in a floating point number
 print(14/7)
+# Use quotient // to get an integer
+print(14//7)
+
 print(+3)
-print(+34)
+print(+34)  # examples of unary plus
 print(+3j)
